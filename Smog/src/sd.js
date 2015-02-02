b9342595ace68fc3887a535dffec82ce753f39c9
Kamil <kapsiakk@gmail.com>
2015-02-02 20:02:32 +0100

    use relative units for fonts show time fix no data bug

--- Smog/src/sd.js
+++ Smog/src/sd.js
@@ -13,26 +13,28 @@ function update(station) {
         var container = document.createElement('div'),
             heading = document.createElement('div'),
             panelBody = document.createElement('div'),
-            value = document.createElement('span');
+            value = document.createElement('span'),
+            time = document.createElement('span');
         container.className = 'panel';
         heading.className = 'panel-heading';
         panelBody.className = 'panel-body';
         value.className = 'value';
+        time.className = 'time';
         panelBody.appendChild(value);
+        panelBody.appendChild(time);
         container.appendChild(heading);
         container.appendChild(panelBody);
         mainContainer.appendChild(container);
     });
 
     function getData() {
-        var req = new XMLHttpRequest();
-        req.onload = function () {
-            var page = document.createElement('html');
-            page.innerHTML = req.responseText;
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            var page = this.responseXML;
             var table = page.querySelector('table tbody');
             var rows = table.querySelectorAll('tr');
-            smogTypes.forEach(function (st, i) {
-                var row = rows[st.row];
+            smogTypes.forEach(function (type, i) {
+                var row = rows[type.row];
                 var j, columns, lastVal, time;
                 columns = [].splice.call(row.querySelectorAll('td'), 0).map(function (col) {
                     return col.textContent;
@@ -40,27 +42,35 @@ function update(station) {
                 for (j = columns.length - 2; j > 0; j--) {
                     lastVal = columns[j].trim();
                     time = j - 2;
-                    if (lastVal) break;
+                    if (lastVal) {
+                        break;
+                    }
                 }
-                var percentVal = Math.round(lastVal / st.norm * 100);
                 var panel = document.querySelectorAll('.panel')[i];
-                if (percentVal >= 100) {
-                    panel.className = 'panel panel-danger';
-                } else if (percentVal >= 75) {
-                    panel.className = 'panel panel-warning';
-                } else {
-                    panel.className = 'panel panel-success';
-                }
-
                 panel.querySelector('.panel-heading').innerHTML =
-                    '<span class="label-long">' + st.label.long + ' (</span>'+
-                    '<span class="label-short">' + st.label.short + '</span>'+
+                    '<span class="label-long">' + type.label.long + ' (</span>' +
+                    '<span class="label-short">' + type.label.short + '</span>' +
                     '<span class="label-long">)';
-                panel.querySelector('.panel-body .value').textContent = percentVal + '%';
+
+                if (time === 0) {
+                    panel.querySelector('.panel-body .value').textContent = 'b/d';
+                } else {
+                    var percentVal = Math.round(lastVal / type.norm * 100);
+                    if (percentVal >= 100) {
+                        panel.className = 'panel panel-danger';
+                    } else if (percentVal >= 75) {
+                        panel.className = 'panel panel-warning';
+                    } else {
+                        panel.className = 'panel panel-success';
+                    }
+                    panel.querySelector('.panel-body .value').textContent = percentVal + '%';
+                    panel.querySelector('.panel-body .time').textContent = time + ':00';
+                }
             });
         };
-        req.open('GET', url);
-        req.send();
+        xhr.open('GET', url);
+        xhr.responseType = "document";
+        xhr.send();
     }
 
     opr.speeddial.update({url: url, title: 'Smog: Kraków - ' + station.name});
