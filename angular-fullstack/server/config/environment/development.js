b2fa4cc63095f983205b2322460f2e9c93b88dba
kapsi <kapsi@o2.pl>
2015-02-24 21:18:19 +0100

    first commit

---
+++ angular-fullstack/server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/test1-dev'
+  },
+
+  seedDB: true
+};
